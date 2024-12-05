ed9123ea257d1b97368597b0c3575571b3068493
mahmud1995 <makhmudgo95@gmail.com>
2024-12-06 07:06:36 +0900

    feat: develop logging via global interceptors

--- apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
@@ -1,17 +1,38 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from "@nestjs/common";
+import { GqlContextType, GqlExecutionContext } from "@nestjs/graphql";
+import { stringify } from "querystring";
 import { Observable } from "rxjs";
 import { tap } from "rxjs/operators";
 
 @Injectable()
 
 export class LoggingInterceptor implements NestInterceptor {
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        console.log("Before...");
+    private readonly logger: Logger = new Logger();
 
-        const now  = Date.now();
-        return next
-        .handle()
-        .pipe(
-            tap(() => console.log(`After... ${Date.now() - now}ms`)));
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        const recordTime  = Date.now();
+        const requestType = context.getType<GqlContextType>();
+        
+        if(requestType === "http") {
+            /* develop if needed! */ 
+
+        } else if ( requestType === "graphql") {
+            /* (1) Print Request */ 
+            const gqlContext = GqlExecutionContext.create(context);
+            console.log("gqlContext => ", gqlContext.getContext().req.body)
+            this.logger.log(`${this.stringify(gqlContext.getContext().req.body)}`, 'REQUEST');
+            /* (2) Error handling via GraphQL */ 
+
+            /* (3) No Errors giving Response below */ 
+            return next.handle().pipe(
+                tap((context) => {
+                    const responseTime = Date.now() - recordTime;
+                    this.logger.log(`${this.stringify(context)} - ${responseTime}ms \n\n`, "RESPONSE");
+                })
+            );
+        }
+    }
+    private stringify(context: ExecutionContext): string {
+        return JSON.stringify(context).slice(0, 75);
     }
 }
