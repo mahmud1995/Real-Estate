43127fb957ecf34e152ea8634c4e734256c73390
mahmud1995 <makhmudgo95@gmail.com>
2024-12-09 18:46:18 +0900

    feat: develop getAllMembersByAdmin and updateMembersByAdmin GrapQL API

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,7 +1,7 @@
 import { Mutation, Resolver, Query, Args, ArgsType } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { UseGuards } from '@nestjs/common';
-import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { AgentsInquiry, LoginInput, MemberInput, MembersInquiry } from '../../libs/dto/member/member.input';
 import { Member, Members } from '../../libs/dto/member/member';
 import { AuthGuard } from "../auth/guards/auth.guard";
 import { AuthMember } from '../auth/decorators/authMember.decorator';
@@ -26,14 +26,14 @@ export class MemberResolver {
     @Mutation(() => Member)
     public async signup(@Args("input") input: MemberInput): Promise<Member> {
         console.log("Mutation: signup");
-        return this.memberService.signup(input);
+        return await this.memberService.signup(input);
     } 
 
     // Authenticated: (USER, AGENT, ADMIN)
     @Mutation(() => Member)
     public async login(@Args('input') input: LoginInput): Promise<Member> {
         console.log("Mutation: login");
-        return this.memberService.login(input);
+        return await this.memberService.login(input);
     } 
 
 
@@ -84,7 +84,7 @@ export class MemberResolver {
         console.log("Query: getMember");
         console.log("memberId:", memberId)
         const targetId = shapeIntoMongoObjectId(input);
-        return this.memberService.getMember(memberId, targetId);
+        return await this.memberService.getMember(memberId, targetId);
     }
 
 
@@ -112,22 +112,27 @@ export class MemberResolver {
         @Args('input') input: AgentsInquiry, 
         @AuthMember('_id') memberId: ObjectId): Promise<Members> {
         console.log('Query: getAgents');
-        return this.memberService.getAgents(memberId, input);
+        return await this.memberService.getAgents(memberId, input);
     }
 
     /** ADMIN */
     @Roles(MemberType.ADMIN)
     @UseGuards(RolesGuard)
-    @Mutation(() => String)
-    public async getAllMembersByAdmin(): Promise<string> {
-        console.log('Mutation: getAllMembersByAdmin');
-        return this.memberService.getAllMembersByAdmin();
+    @Query(() => Members)
+    public async getAllMembersByAdmin(@Args('input') input: MembersInquiry): Promise<Members> {
+        console.log('Query: getAllMembersByAdmin');
+        return await this.memberService.getAllMembersByAdmin(input);
     }
+    
     // Authorization: ADMIN
-    @Mutation(() => String)
-    public async updateMembersByAdmin(): Promise<string> {
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation(() => Member)
+    public async updateMembersByAdmin(@Args("input") input: MemberUpdate): Promise<Member> {
         console.log('Mutation: updateMembersByAdmin');
-        return this.memberService.updateMembersByAdmin();
+        return await this.memberService.updateMembersByAdmin(input);
     }
 
 }
+
+
