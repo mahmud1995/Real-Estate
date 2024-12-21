2549631a1a8f1decdacf5a0450e9b1006083ab06
mahmud1995 <makhmudgo95@gmail.com>
2024-12-22 01:20:43 +0900

    feat: develop likeTargetProperty GraphQL API

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -6,13 +6,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
 import { MemberModule } from '../member/member.module';
+import { LikeModule } from '../like/like.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: "Property", schema: PropertySchema}]),
     AuthModule, // AuthModule da UseGuards larni chaqirib olamiz
     ViewModule, // viewmodule ga tegishli viewServiceni bemalol ishlatamiz
-    MemberModule
+    MemberModule,
+    LikeModule,
   ],
 
   providers: [PropertyResolver, PropertyService],
