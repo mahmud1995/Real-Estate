90ccc8a1da5a9a0bb14c9b2f5cdee8a71a94ba7e
mahmud1995 <makhmudgo95@gmail.com>
2024-12-22 23:39:11 +0900

    feat: develop subscribe, unsubscribe, getMemberFollowings and getMemberFollowers GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -18,6 +18,7 @@ export const availableCommentSorts = ['createdAt', 'updatedAt'];
 /** IMAGE CONFIGURATION **/
 import { v4 as uuidv4 } from "uuid";
 import * as path from "path";
+import { T } from "./types/common";
 
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = ( filename: string ) => {
@@ -25,10 +26,81 @@ export const getSerialForImage = ( filename: string ) => {
     return uuidv4() + ext;
 }; 
 
-
 export const shapeIntoMongoObjectId = (target: any) => {
     return typeof target === 'string' ? new ObjectId(target) : target;
-}
+};
+
+export const lookupAuthMemberLiked = (memberId: T, targetRefId: string = '$_id') =>{
+    return {
+        $lookup: {
+            from: 'likes',
+            let: {
+                localLikeRefId: targetRefId,
+                localMemberId: memberId,
+                localMyFavorite: true
+            },
+            pipeline: [
+                {
+                    $match: {
+                        $expr: {
+                            $and: [
+                                {$eq: ["$likeRefId", "$$localLikeRefId"]}, 
+                            {$eq: ["$memberId", "$$localMemberId"]}
+                        ],
+                        },
+                    },
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        memberId: 1,
+                        likeRefId: 1,
+                        myFavorite: "$$localMyFavorite",
+                    },
+                },
+            ],
+            as: "meLiked",
+        },
+    };
+};
+interface LookupAuthMemberFollowed {
+    followerId: T;
+    followingId: string;
+};
+
+
+export const lookupAuthMemberFollowed = (input: LookupAuthMemberFollowed) =>{
+    const {followerId, followingId} = input;
+    return {
+        $lookup: {
+            from: 'follows',
+            let: {
+                localFollowerId: followerId,
+                localFollowingId: followingId,
+                localMyFavorite: true
+            },
+            pipeline: [
+                {
+                    $match: {
+                        $expr: {
+                            $and: [{ $eq: ["$followerId", "$$localFollowerId"]}, {$eq: ["$followingId", "$$localFollowingId"] }],
+                        },
+                    },
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        followerId: 1,
+                        followingId: 1,
+                        myFollowing: "$$localMyFavorite",
+                    },
+                },
+            ],
+            as: "meFollowed",
+        },
+    };
+};
+
 
 export const lookupMember = {
     $lookup: {
@@ -37,4 +109,22 @@ export const lookupMember = {
         foreignField: '_id',
         as: 'memberData',
     },
-}
+};
+
+export const lookupFollowingData = {
+    $lookup: {
+        from: 'members',
+        localField: 'followingId',
+        foreignField: '_id',
+        as: 'followingData',
+    },
+};
+
+export const lookupFollowerData = {
+    $lookup: {
+        from: 'members',
+        localField: 'followerId',
+        foreignField: '_id',
+        as: 'followerData',
+    },
+};
